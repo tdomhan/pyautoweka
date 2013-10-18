dc9ba0c1d1ad19cc928ceb6e770894f239476527
Tobias Domhan <tdomhan@gmail.com>
2013-10-18 17:07:22 +0200

    restructed experiment layout

--- pyautoweka.py
+++ pyautoweka.py
@@ -51,6 +51,20 @@ class RandomSubSampling(InstanceGenerator):
         if bias_to_uniform:
             self.params["bias"] = bias_to_uniform
 
+class DataSet(object):
+    def __init__(self, train_file, test_file=None, name="data"):
+        """
+        Dataset.
+
+        :param train_file: ARFF file containing the training data
+        :param test_file: ARFF file containing the testing data, that will be
+        used once the experiment completed (optional)
+        :param name: name of the dataset (optional)
+        """
+        self.train_file = train_file
+        self.test_file = test_file
+        self.name = name
+
 
 class Experiment:
 
@@ -83,13 +97,131 @@ class Experiment:
         "TPE": ""
         }
 
-    def __init__(self):
-        self.root = ET.Element('experimentBatch')
-        self.tree = ET.ElementTree(self.root)
+    def __init__(
+            self,
+            experiment_name="Experiment",
+            result_metric=RESULT_METRICS[0],
+            optimization_method=OPTIMIZATION_METHOD[0],
+            instance_generator=None,
+            tuner_timeout=180,
+            train_timeout=120,
+            attribute_selection=True,
+            attribute_selection_timeout=100,
+            memory="3000m"
+            ):
+        """
+        Create a new experiment.
 
-        self.set_experiment_parameters()
+        :param tuner_timeout: The number of seconds to run the SMBO method.
+        :param train_timeout: The number of seconds to spend training
+        a classifier with a set of hyperparameters on a given partition of
+        the training set.
+        """
+        if result_metric not in Experiment.RESULT_METRICS:
+            raise ValueError("%s is not a valid result metric,"
+                             " choose one from: %s" % (
+                                 result_metric,
+                                 ", ".join(Experiment.RESULT_METRICS)))
 
-    def set_data_set(self, train_file, test_file=None, name="data"):
+        if optimization_method not in Experiment.OPTIMIZATION_METHOD:
+            raise ValueError("%s is not a valid result metric,"
+                             " choose one from:" % (
+                                 optimization_method,
+                                 ", ".join(Experiment.OPTIMIZATION_METHOD)))
+
+        if (instance_generator
+                and not isinstance(instance_generator, InstanceGenerator)):
+            raise ValueError(("instance_generator needs to be"
+                              " an InstanceGenerator or None"))
+
+        if not isinstance(attribute_selection, bool):
+            raise ValueError("attribute_selection needs to be a boolean")
+
+        self.experiment_name = experiment_name
+        self.result_metric = result_metric
+        self.optimization_method = optimization_method
+        self.instance_generator = instance_generator
+        self.tuner_timeout = tuner_timeout
+        self.train_timeout = train_timeout
+        self.attribute_selection = attribute_selection
+        self.attribute_selection_timeout = attribute_selection_timeout
+        self.memory = memory
+
+        self.datasets = []
+
+    def write_xml(self, file_name="experiment.xml"):
+        """
+        Write this experiment as a valid xml that can be read by Auto-WEKA.
+        """
+        root = ET.Element('experimentBatch')
+        tree = ET.ElementTree(root)
+
+        experiment = ET.SubElement(root, 'experimentComponent')
+        name_node = ET.SubElement(experiment, 'name')
+        name_node.text = self.experiment_name
+
+        experiment_constructor = ET.SubElement(root,
+                                               'experimentConstructor')
+        experiment_constructor.text = Experiment.OPTIMIZATION_METHOD_CONSTRUCTOR[
+            self.optimization_method]
+        for experiment_arg in Experiment.OPTIMIZATION_METHOD_ARGS[
+                self.optimization_method]:
+            experiment_arg_node = ET.SubElement(experiment,
+                                                'experimentConstructorArgs')
+            experiment_arg_node.text = experiment_arg
+
+        extra_props_node = ET.SubElement(experiment, 'extraProps')
+        extra_props_node.text = Experiment.OPTIMIZATION_METHOD_EXTRA[
+            self.optimization_method]
+
+        instance_generator_node = ET.SubElement(experiment,
+                                                'instanceGenerator')
+        if not self.instance_generator:
+            #Default generator
+            instance_generator_node.text = "autoweka.instancegenerators.Default"
+        else:
+            instance_generator_node.text = self.instance_generator.name
+            instance_generator_args_node = ET.SubElement(
+                    experiment,
+                    'instanceGeneratorArgs')
+            instance_generator_args_node.text = self.instance_generator.get_arg_str()
+
+        tuner_timeout_node = ET.SubElement(experiment, 'tunerTimeout')
+        tuner_timeout_node.text = str(self.tuner_timeout)
+        train_timeout_node = ET.SubElement(experiment, 'trainTimeout')
+        train_timeout_node.text = str(self.train_timeout)
+            
+        attribute_selection_node = ET.SubElement(experiment, 'attributeSelection')
+        if self.attribute_selection:
+            attribute_selection_node.text = "true"
+            attr_select_timeout_node = ET.SubElement(
+                    experiment, 'attributeSelectionTimeout')
+            attr_select_timeout_node.text = str(self.attribute_selection_timeout)
+        else:
+            attribute_selection_node.text = "false"
+
+        memory_node = ET.SubElement(experiment, 'memory')
+        memory_node.text = str(self.memory)
+
+        # Write all dataset components:
+
+        for dataset in self.datasets:
+            dataset_node = ET.SubElement(root, 'datasetComponent')
+            train_file_node = ET.SubElement(dataset_node, 'trainArff')
+            train_file_node.text = dataset.train_file
+            test_file_node = ET.SubElement(dataset_node, 'testArff')
+            if dataset.test_file:
+                test_file_node.text = dataset.test_file
+            else:
+                #train_file not set, so use the train file again
+                test_file_node.text = dataset.train_file
+            name_node = ET.SubElement(dataset_node, 'name')
+            name_node.text = dataset.name
+
+        tree.write(file_name)
+
+
+    def add_data_set(self, train_file, test_file=None, name="data"):
         """
         Add a dataset to the experiment.
 
@@ -98,109 +230,4 @@ class Experiment:
         used once the experiment completed (optional)
         :param name: name of the dataset (optional)
         """
-        if self.root.find("datasetComponent"):
-            self.root.remove(self.root.find("datasetComponent"))
-
-        dataset_node = ET.SubElement(self.root, 'datasetComponent')
-        train_file_node = ET.SubElement(dataset_node, 'trainArff')
-        train_file_node.text = train_file
-        test_file_node = ET.SubElement(dataset_node, 'testArff')
-        if test_file:
-            test_file_node.text = test_file
-        else:
-            #train_file not set, so use the train file again
-            test_file_node.text = train_file
-        name_node = ET.SubElement(dataset_node, 'name')
-        name_node.text = name
-
-    def set_experiment_parameters(
-            self,
-            name="Experiment",
-            result_metric=RESULT_METRICS[0],
-            optimization_method=OPTIMIZATION_METHOD[0],
-            instance_generator=None,
-            tuner_timeout=180,
-            train_timeout=120,
-            attribute_selection=True,
-            attribute_selection_timeout=100
-            ):
-        """
-        Set the parameters of the experiment.
-
-        :param tuner_timeout: The number of seconds to run the SMBO method.
-        :param train_timeout: The number of seconds to spend training a classifier with a set of hyperparameters
-        on a given partition of the training set.
-        """
-        if result_metric not in Experiment.RESULT_METRICS:
-            print ("FAILED: %s is not a valid result metric,"
-                   " choose one from:" % result_metric)
-            print ", ".join(Experiment.RESULT_METRICS)
-            return
-
-        if optimization_method not in Experiment.OPTIMIZATION_METHOD:
-            print ("FAILED: %s is not a valid result metric,"
-                   " choose one from:" % optimization_method)
-            print ", ".join(Experiment.OPTIMIZATION_METHOD)
-            return
-
-        if (instance_generator
-                and not isinstance(instance_generator, InstanceGenerator)):
-            print "FAILED: instance_generator is not an InstanceGenerator"
-            return
-
-        if not isinstance(attribute_selection, bool):
-            print "FAILED: attribute_selection needs to be a boolean"
-            return
-
-        #clear previous parameters:
-        if self.root.find("selfComponent"):
-            self.root.remove(self.root.find("selfComponent"))
-
-        experiment = ET.SubElement(self.root, 'experimentComponent')
-        name_node = ET.SubElement(experiment, 'name')
-        name_node.text = name
-
-        experiment_constructor = ET.SubElement(self.root,
-                                               'experimentConstructor')
-        experiment_constructor.text = Experiment.OPTIMIZATION_METHOD_CONSTRUCTOR[
-                optimization_method]
-        for experiment_arg in Experiment.OPTIMIZATION_METHOD_ARGS[
-                optimization_method]:
-            experiment_arg_node = ET.SubElement(experiment,
-                                                'experimentConstructorArgs')
-            experiment_arg_node.text = experiment_arg
-
-        extra_props_node = ET.SubElement(experiment, 'extraProps')
-        extra_props_node.text = Experiment.OPTIMIZATION_METHOD_EXTRA[
-                optimization_method]
-
-        instance_generator_node = ET.SubElement(experiment,
-                                               'instanceGenerator')
-        if not instance_generator:
-            #Default generator
-            instance_generator_node.text = "autoweka.instancegenerators.Default"
-        else:
-            instance_generator_node.text = instance_generator.name
-            instance_generator_args_node = ET.SubElement(experiment,
-                                                         'instanceGeneratorArgs')
-            instance_generator_args_node.text = instance_generator.get_arg_str()
-
-        tuner_timeout_node = ET.SubElement(experiment, 'tunerTimeout')
-        tuner_timeout_node.text = str(tuner_timeout)
-        train_timeout_node = ET.SubElement(experiment, 'trainTimeout')
-        train_timeout_node.text = str(train_timeout)
-            
-        attribute_selection_node = ET.SubElement(experiment, 'attributeSelection')
-        if attribute_selection:
-            attribute_selection_node.text = "true"
-            #TODO: attribute_selection_timeout 
-        else:
-            attribute_selection_node.text = "false"
-
-        memory_node = ET.SubElement(experiment, 'memory')
-        memory_node.text = "3000m"
-
-
-
-    def write(self, file_name="experiment.xml"):
-        self.tree.write(file_name)
+        self.datasets.append(DataSet(train_file, test_file, name))
